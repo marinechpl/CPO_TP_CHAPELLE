7fed751c54e54398071426d0c8514fb1dd165cba
Marine Chapelle <marine.chapelle@epfedu.fr>
2022-10-21 17:34:32 +0200

    Partie 2 TP

---
+++ TP2_convertisseurObjet_CHAPELLE/src/tp2_convertisseurobjet_chapelle/Convertisseur.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_chapelle;
+
+/**
+ *
+ * @author 33769
+ */
+public class Convertisseur {
+    int nbConversions;
+    
+    public void Convertisseur(){
+        
+    }
+    
+    public double CelciusVersKelvin (){
+        
+    }
+    
+    public double KelvinVersCelcius(){
+        
+    }
+}
+
+
---
+++ TP2_convertisseurObjet_CHAPELLE/src/tp2_convertisseurobjet_chapelle/TP2_convertisseurObjet_CHAPELLE.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseurobjet_chapelle;
+
+/**
+ *
+ * @author 33769
+ */
+public class TP2_convertisseurObjet_CHAPELLE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
