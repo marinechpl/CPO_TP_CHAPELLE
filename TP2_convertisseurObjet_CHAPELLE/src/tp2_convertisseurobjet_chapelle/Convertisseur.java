68da281fe503c93f24e84eaca38e46e9cd6335d6
Marine Chapelle <marine.chapelle@epfedu.fr>
2022-10-25 12:18:03 +0200

    Partie 2 du TP2 : affichage du menu avec les choix utilisateurs et l'affichage de la conversion

--- TP2_convertisseurObjet_CHAPELLE/src/tp2_convertisseurobjet_chapelle/Convertisseur.java
+++ TP2_convertisseurObjet_CHAPELLE/src/tp2_convertisseurobjet_chapelle/Convertisseur.java
@@ -9,14 +9,14 @@ package tp2_convertisseurobjet_chapelle;
  * @author 33769
  */
 public class Convertisseur {
-    int nbConversions;
+    int nbConversions; //création de la variable de comptage 
     
     public void Convertisseur(){   
     }
     
     public double CelciusVersKelvin (double C){
         double K= C+273.15;
-        nbConversions+=1;
+        nbConversions+=1; //ajout d'un car une nouvelle conversion est effectuée 
         return K;
     }
     
@@ -27,32 +27,32 @@ public class Convertisseur {
     }
     
     public double FarenheitVersCelcius(double F){    
-        double C=(F-32)*(5/9);
+        double C=(F-32)* 5/9;
         nbConversions+=1;
         return C;
     }
     
     public double CelciusVersFarenheit(double C){    
-        double F = (C*(9/5)) + 32 ;
+        double F = (C * 9/5) + 32 ;
         nbConversions+=1;
         return F;
     }
     
     public double FarenheitVersKelvin(double F){   
-        double K = (F-32)*(5/9) + 273.15;
+        double K = (F-32)* 5/9 + 273.15;
         nbConversions+=1;
         return K;
     }
     
     public double KelvinVersFarenheit(double K){   
-         double F = (K-273.15)*(9/5) + 32;
+         double F = (K-273.15)* 9/5 + 32;
          nbConversions+=1;
          return F;
     }
     
     @Override
     public String toString () {
-    return "nb de conversions : "+ nbConversions;
+    return "nb de conversions : "+ nbConversions; //affichage du nombre de conversion totale 
     }
     
 }
--- TP2_convertisseurObjet_CHAPELLE/src/tp2_convertisseurobjet_chapelle/TP2_convertisseurObjet_CHAPELLE.java
+++ TP2_convertisseurObjet_CHAPELLE/src/tp2_convertisseurobjet_chapelle/TP2_convertisseurObjet_CHAPELLE.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tp2_convertisseurobjet_chapelle;
-
+import java.util.Scanner;
 /**
  *
  * @author 33769
@@ -14,21 +14,58 @@ public class TP2_convertisseurObjet_CHAPELLE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Convertisseur Conversion1 = new Convertisseur();
-        Conversion1.CelciusVersFarenheit(32);
-        Conversion1.KelvinVersCelcius(273.15);
-        Conversion1.KelvinVersCelcius(1893);
-        System.out.println(Conversion1);
+        Convertisseur Conversion = new Convertisseur();
+        //Conversion1.CelciusVersFarenheit(32);
+        //Conversion1.KelvinVersCelcius(273.15);
+        //Conversion1.KelvinVersCelcius(1893);
+        //System.out.println(Conversion1);
         
-        Convertisseur Conversion2 = new Convertisseur();
-        Conversion2.KelvinVersFarenheit(372);
-        Conversion2.CelciusVersKelvin(54);
-        Conversion2.FarenheitVersCelcius(647);
-        System.out.println(Conversion2); 
+        //Convertisseur Conversion2 = new Convertisseur();
+        //Conversion2.KelvinVersFarenheit(372);
+        //Conversion2.CelciusVersKelvin(54);
+        //Conversion2.FarenheitVersCelcius(647);
+        //System.out.println(Conversion2); 
         
+        System.out.println("""
+                           1.Celcius vers Farenheit
+                           2.Celcius vers Kelvin
+                           3.Kelvin vers Celcius
+                           4.Kelvin vers Farenheit
+                           5.Farenheit Vers Celcius
+                           6.Farenheit vers Kelvin""");
         
-        
-    }
-    
-    
+        System.out.println("Entrez votre choix : ");
+        Scanner saisie = new Scanner(System.in);
+        double choix = saisie.nextDouble(); //création de la variable choix pour les entrées utilisateur
+        if (choix==1){ //différent type de choix : les définir un par un 
+            System.out.println("Entrer une valeur:");
+            double temp=saisie.nextDouble(); //création val=riable température 
+            System.out.println("Valeur en Farenheit ="+ Conversion.CelciusVersFarenheit(temp)); //affciher le message avec le renvoie a la méthode dans l'objet pour convertir 
+            }
+        else if (choix==2){
+            System.out.println("Entrer une valeur:");
+            double temp=saisie.nextDouble();
+            System.out.println("Valeur en Kelvin ="+ Conversion.CelciusVersKelvin(temp));
+            }
+        else if (choix==3){
+            System.out.println("Entrer une valeur:");
+            double temp =saisie.nextDouble();
+            System.out.println("Valeur en Celcius ="+ Conversion.KelvinVersCelcius(temp));
+            }
+        else if (choix==4){
+            System.out.println("Entrer une valeur:");
+            double temp =saisie.nextDouble();
+            System.out.println("Valeur en Farenheit ="+ Conversion.KelvinVersFarenheit(temp));
+        }
+        else if (choix==5){
+            System.out.println("Entrer une valeur:");
+            double temp =saisie.nextDouble();
+            System.out.println("Valeur en Celcius ="+ Conversion.FarenheitVersCelcius(temp));
+        }
+        else if (choix==6){
+            System.out.println("Entrer une valeur:");
+            double temp =saisie.nextDouble();
+            System.out.println("Valeur en Kelvin ="+ Conversion.FarenheitVersKelvin(temp));
+        }
+        }
 }
