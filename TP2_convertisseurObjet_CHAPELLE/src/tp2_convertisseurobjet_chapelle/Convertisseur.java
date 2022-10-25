61ff4e09971605bfc87df4b94fcdc518bced7e48
Marine Chapelle <marine.chapelle@epfedu.fr>
2022-10-25 10:41:22 +0200

    Partie 2 du TP2 : objet et méthode pour convertir OK -> menu à développer

--- TP2_convertisseurObjet_CHAPELLE/src/tp2_convertisseurobjet_chapelle/Convertisseur.java
+++ TP2_convertisseurObjet_CHAPELLE/src/tp2_convertisseurobjet_chapelle/Convertisseur.java
@@ -11,17 +11,50 @@ package tp2_convertisseurobjet_chapelle;
 public class Convertisseur {
     int nbConversions;
     
-    public void Convertisseur(){
-        
+    public void Convertisseur(){   
     }
     
-    public double CelciusVersKelvin (){
-        
+    public double CelciusVersKelvin (double C){
+        double K= C+273.15;
+        nbConversions+=1;
+        return K;
     }
     
-    public double KelvinVersCelcius(){
-        
+    public double KelvinVersCelcius(double K){   
+        double C= K-273.15;
+        nbConversions+=1;
+        return C;
     }
+    
+    public double FarenheitVersCelcius(double F){    
+        double C=(F-32)*(5/9);
+        nbConversions+=1;
+        return C;
+    }
+    
+    public double CelciusVersFarenheit(double C){    
+        double F = (C*(9/5)) + 32 ;
+        nbConversions+=1;
+        return F;
+    }
+    
+    public double FarenheitVersKelvin(double F){   
+        double K = (F-32)*(5/9) + 273.15;
+        nbConversions+=1;
+        return K;
+    }
+    
+    public double KelvinVersFarenheit(double K){   
+         double F = (K-273.15)*(9/5) + 32;
+         nbConversions+=1;
+         return F;
+    }
+    
+    @Override
+    public String toString () {
+    return "nb de conversions : "+ nbConversions;
+    }
+    
 }
 
 
--- TP2_convertisseurObjet_CHAPELLE/src/tp2_convertisseurobjet_chapelle/TP2_convertisseurObjet_CHAPELLE.java
+++ TP2_convertisseurObjet_CHAPELLE/src/tp2_convertisseurobjet_chapelle/TP2_convertisseurObjet_CHAPELLE.java
@@ -14,7 +14,21 @@ public class TP2_convertisseurObjet_CHAPELLE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Convertisseur Conversion1 = new Convertisseur();
+        Conversion1.CelciusVersFarenheit(32);
+        Conversion1.KelvinVersCelcius(273.15);
+        Conversion1.KelvinVersCelcius(1893);
+        System.out.println(Conversion1);
+        
+        Convertisseur Conversion2 = new Convertisseur();
+        Conversion2.KelvinVersFarenheit(372);
+        Conversion2.CelciusVersKelvin(54);
+        Conversion2.FarenheitVersCelcius(647);
+        System.out.println(Conversion2); 
+        
+        
         
     }
+    
     
 }
