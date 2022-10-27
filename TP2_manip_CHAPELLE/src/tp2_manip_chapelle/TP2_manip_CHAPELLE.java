1fe38093f78e8482eca6a527ab217e6e7a876185
Marine Chapelle <marine.chapelle@epfedu.fr>
2022-10-27 20:58:48 +0200

    partie 3 TP 2

--- TP2_manip_CHAPELLE/src/tp2_manip_chapelle/TP2_manip_CHAPELLE.java
+++ TP2_manip_CHAPELLE/src/tp2_manip_chapelle/TP2_manip_CHAPELLE.java
@@ -19,10 +19,15 @@ public class TP2_manip_CHAPELLE {
         Tartiflette assiette2 = new Tartiflette(700);
         Tartiflette assiette3 = assiette2 ; 
         assiette2=assiette1;
-        assiette1=assiette3;
+        assiette1=assiette3; //échange des valeurs des 2 assiettes 
         
         System.out.println("nb de calories de Assiette 2 : " +assiette2.nbCalories) ;
         System.out.println("nb de calories de Assiette 1 : " +assiette1.nbCalories) ;
+        
+        
+        Moussaka Tab [] = new Moussaka[10]; //création du tableau avec 1à cases 
+        for (int i=0;i<10;i++){ //boucle pour les 10 cases du tableau
+        Tab[i]=new Moussaka(i);
+        }
     }
-    
 }
